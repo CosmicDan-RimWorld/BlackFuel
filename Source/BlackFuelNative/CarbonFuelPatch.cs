3be2cdc7ca5829eb41e05fe832efa8a82db65925
CosmicDan <dconnolly.au@gmail.com>
2017-06-29 15:48:44 +1000

    Native - Carbon fuel sources are 3x as potent as Wood Logs

---
+++ Source/BlackFuelNative/CarbonFuelPatch.cs
@@ -0,0 +1,48 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using Verse;
+using Verse.AI;
+
+namespace BlackFuelNative
+{
+    // hook the job for getting fuel - if carbon item is designated, divide the count (for delivery) by 3
+    [HarmonyPatch(typeof(WorkGiver_Refuel))]
+    [HarmonyPatch("JobOnThing")]
+    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(Thing), typeof(bool) })]
+    class WorkGiver_Refuel__JobOnThing
+    {
+        static void Postfix(WorkGiver_Refuel __instance, ref Job __result)
+        {
+            if (Util.IsCarbonThing(__result.targetB.Thing))
+                __result.count /= 3;
+        }
+
+    }
+
+    // hook the actual fuel insertion action - if carbon item is added, multiply it by 3
+    [HarmonyPatch(typeof(CompRefuelable))]
+    [HarmonyPatch("Refuel")]
+    [HarmonyPatch(new Type[] { typeof(Thing) })]
+    class PatchCompRefuelable__Refuel
+    {
+        static void Prefix(CompRefuelable __instance, ref Thing fuelThing)
+        {
+            if (Util.IsCarbonThing(fuelThing))
+                fuelThing.stackCount *= 3;
+        }
+
+    }
+
+    public class Util
+    {
+        public static bool IsCarbonThing(Thing thing)
+        {
+            foreach (var thingCategory in thing.def.thingCategories)
+                if (thingCategory.defName.Equals("Carbon"))
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ Source/BlackFuelNative/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Harmony;
+using Verse;
+using System.Reflection;
+
+namespace BlackFuelNative
+{
+    public class Main : Mod
+    {
+        public Main(ModContentPack content) : base(content)
+        {
+            var harmony = HarmonyInstance.Create("com.cosmicdan.rimworld.blackfuelnative");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("BlackFuel mod native has loaded!");
+        }
+    }
+}
